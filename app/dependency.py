c643032fa8892df95243ef1922d4c372197091be
Elya_1011 <elya.alimova.01@mail.ru>
2026-01-30 17:39:52 +0300

    add new dependency

--- app/dependency.py
+++ app/dependency.py
@@ -1,7 +1,10 @@
 from typing import Annotated
-from fastapi import Depends
-from models import Session
+from fastapi import Depends, Header, HTTPException
+from models import Session, Token
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+import uuid, datetime
+from config import TOKEN_TTL_SEC
 
 
 async def get_session() -> AsyncSession:
@@ -10,3 +13,20 @@ async def get_session() -> AsyncSession:
 
 
 SessionDependency = Annotated[AsyncSession, Depends(get_session)]
+
+
+async def get_token(
+    x_token: Annotated[uuid.UUID, Header()], session: SessionDependency
+) -> Token:
+    query = select(Token).where(
+        Token.token == x_token,
+        Token.creation_time
+        >= (datetime.datetime.now() - datetime.timedelta(seconds=TOKEN_TTL_SEC)),
+    )
+    token = await session.scalar(query)
+    if token is None:
+        raise HTTPException(401, "Token not found")
+    return token
+
+
+TokenDependency = Annotated[Token, Depends(get_token)]
