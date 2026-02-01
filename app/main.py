982ab88ed76825e058ad619376eda7c7f4552f69
Elya_1011 <elya.alimova.01@mail.ru>
2026-02-01 15:22:01 +0300

    add funcs for login, patch, delete and get users and add token in post adv

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+from security import check_password
 from fastapi import FastAPI, HTTPException
 from lifespan import lifespan
-from dependency import SessionDependency, TokenDependency
+from dependency import SessionDependency, TokenDependency, CurrentUserDependency
 from schemes import CreateAdvertisementRequest, SearchAdvResponse, UpdateAdvertisement, \
-    GetAdvertisement, CreateUserRequest, CreateUserResponse
+    GetAdvertisement, CreateUserRequest, LoginResponse, LoginRequest, UpdateUser
 import models, crud
 from sqlalchemy import select, func, or_, and_
 from typing import Optional
@@ -14,7 +16,7 @@ app = FastAPI(
 )
 
 
-@app.post('/users', status_code=201)
+@app.post('/user', tags=['user'], status_code=201)
 async def create_user(user: CreateUserRequest, session: SessionDependency):
     query = select(models.User).where(models.User.email == user.email)
     result = await session.scalars(query)
@@ -28,12 +30,75 @@ async def create_user(user: CreateUserRequest, session: SessionDependency):
     )
     await user_orm_obj.set_password(user.password)
     await crud.add_item(session, user_orm_obj)
+    return user_orm_obj.dict
+
+
+@app.post('/login', tags=['login'], status_code=200, response_model=LoginResponse)
+async def login_user(login_data: LoginRequest, session: SessionDependency):
+    query = select(models.User).where(models.User.email == login_data.email)
+    user =await session.scalar(query)
+    if user is None:
+        raise HTTPException(401, 'Invalid credentials')
+    if not await check_password(login_data.password, user.password_hash):
+        raise HTTPException(401, 'Invalid credentials')
+    token = models.Token(user_id=user.id)
+    await crud.add_item(session, token)
+    return token.dict
+
+
+@app.patch('/user/{user_id}', tags=['user'], status_code=200)
+async def edit_user(
+        user_id: int,
+        data: UpdateUser,
+        session: SessionDependency,
+        token: TokenDependency,
+        current_user: CurrentUserDependency
+):
+    if current_user.id != user_id and not getattr(current_user, 'admin', False):
+        raise HTTPException(status_code=403, detail="you don't have permission to edit this user")
+    query = select(models.User).where(models.User.id == user_id)
+    user_to_edit = await session.scalar(query)
+    if not user_to_edit:
+        raise HTTPException(status_code=404, detail=f'User with ID {user_id} not found')
+
+    if data.email and data.email != user_to_edit.email:
+        query = select(models.User).where(models.User.email == data.email, models.User.id != user_id)
+        existing_user = await session.scalar(query)
+
+        if existing_user:
+            raise HTTPException(status_code=400, detail='email already registered')
+
+    update_data = data.dict(exclude_unset=True)
+    if update_data:
+        for field, value in update_data.items():
+            setattr(user_to_edit, field, value)
+
+        user_to_edit.updated_at = datetime.utcnow()
+        await session.commit()
+        await session.refresh(user_to_edit)
+    return user_to_edit.dict
+
+
+@app.get('/user/{user_id}', tags=['user'], status_code=200)
+async def get_user(user_id: int, session: SessionDependency):
+    user_orm_obj = await crud.get_item_by_id(session, models.User, user_id)
+    return user_orm_obj.dict
+
+
+@app.delete('/user/{user_id}', tags=['user'], status_code=204)
+async def delete_user(user_id: int, session: SessionDependency, token: TokenDependency, current_user: CurrentUserDependency):
+    user_orm_obj = await crud.get_item_by_id(session, models.User, user_id)
+    if not user_orm_obj:
+        raise HTTPException(status_code=404, detail='user not found')
+    if current_user.role != 'admin' and current_user.id != user_id:
+        raise HTTPException(403, 'You can only delete your own account')
+    await crud.delete_item(session, user_orm_obj)
 
 
 @app.post('/advertisements', status_code=201)
 async def create_advertisement(adv: CreateAdvertisementRequest, session: SessionDependency, token: TokenDependency):
     adv_dict = adv.model_dump(exclude_unset=True)
-    adv_orm_obj = models.Advertisement(**adv_dict)
+    adv_orm_obj = models.Advertisement(**adv_dict, user_id=token.user_id)
     await crud.add_item(session, adv_orm_obj)
     return adv_orm_obj.dict
 
@@ -107,7 +172,7 @@ async def search_advertisement(
 
 
 
-@app.patch('/advertisements/{adv_id}', response_model=GetAdvertisement)
+@app.patch('/advertisements/{adv_id}', response_model=GetAdvertisement) # add token
 async def update_advertisement(adv_id: int, adv_data: UpdateAdvertisement, session: SessionDependency):
     query = select(models.Advertisement).where(models.Advertisement.id == adv_id)
     result = await session.scalars(query)
@@ -147,7 +212,7 @@ async def update_advertisement(adv_id: int, adv_data: UpdateAdvertisement, sessi
     }
 
 
-@app.delete('/advertisements/{adv_id}', status_code=204)
+@app.delete('/advertisements/{adv_id}', status_code=204) # add token
 async def delete_advertisement(adv_id: int, session: SessionDependency):
     adv_orm_obj = await crud.get_item_by_id(session, models.Advertisement, adv_id)
     if not adv_orm_obj:
